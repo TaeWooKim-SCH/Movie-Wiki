c5aa4d9551764015f819d640b63f8c726764cd63
KimJunpyo <bono1725@naver.com>
2023-05-08 13:45:56 +0900

    feat: tailwind-styled-components
    1. tailwind-styled-components를 설정 완료 했습니다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.css';
+import tw from 'tailwind-styled-components';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Test>1</Test>
+    </div>
+  );
 }
+
+const Test = tw.div`
+  font-black
+  text-xl
+`;
 
 export default App;
