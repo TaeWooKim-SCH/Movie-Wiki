b9b781f462443498c73c5f3cda7aa9dd3e0c3a0f
zop1234 <zop1234@hanmail.net>
2023-05-12 15:46:57 +0900

    feat: NavBar
    1. 네브바를 만들었습니다.
    2. 테일윈드에 색깔을 추가했습니다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+// import tw from 'tailwind-styled-components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import Search from './Pages/Search';
+import Category from './Pages/Category';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/category" element={<Category />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
+
+// const Test = tw.div`
+//   font-black
+//   text-xl
+// `;
 
 export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import Navbar from '../Components/Navbar';
+
+export default function Home() {
+  return (
+    <section id="home" className="ml-56">
+      <h1>Home</h1>
+      <h2>Home</h2>
+    </section>
+  );
+}
