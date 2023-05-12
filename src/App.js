c517c3aa328ec705beacf4f8fba05ee19e9fbc50
KimJunpyo <bono1725@naver.com>
2023-05-12 16:04:49 +0900

    refactor: 디렉토리 구조 변경
    1. 불필요한 파일을 삭제했습니다.
    2. jsx -> js로 레이아웃이 없는 파일의  파일 확장자를 변경했습니다.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import MovieCard from './Components/MovieCard';
-import movieData from './API/movie';
-
-const API = process.env.REACT_APP_TMDB_API_KEY;
+import React from 'react';
 
 function App() {
-  const [test, setTest] = useState('');
-  useEffect(() => {
-    movieData(API)
-      .then(response => response.json())
-      .then(response => setTest(response))
-      .catch(err => console.error(err));
-  }, []);
-
-  return (
-    <div className="App">
-      <div className="h-full w-full flex flex-wrap justify-around bg-backgroundNormal">
-        {test &&
-          test.results.map(e => {
-            return <MovieCard key={e.id} movie={e} />;
-          })}
-      </div>
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
